b1a57b71e860a575820f52ed6b873ff3abd685a5
FSV.lab <fsaldana7@misena.edu.co>
2022-11-08 15:28:28 -0500

    proyecto apple

---
+++ src/apple/Apple.java
@@ -0,0 +1,22 @@
+
+
+package apple;
+
+import java.util.Scanner;
+
+
+public class Apple {
+   
+
+    
+    public static void main(String[] args) {
+      
+        
+        
+        Boton llamar = new Boton();
+        llamar.setVisible(true);
+        
+        
+    }
+    
+}
